f921959234f7b0845550028a312031873d8f3501
Ilona R <ilona.raugalaite@yahoo.co.uk>
2023-04-16 16:18:26 +0200

    created component Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import Router from './routes/Router';
+import Header from './components/shared/Header'
 
 function App() {
   return (
-    <Router />
+    <div>
+      <Header />
+      <Router />
+    </div>
   );
 }
 
