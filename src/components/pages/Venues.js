9c613511c8b4236b90bb46df49133fa64582e28a
Ilona R <ilona.raugalaite@yahoo.co.uk>
2023-04-17 02:35:47 +0200

    HOT-18 fetchVenues, display list to user

--- src/components/pages/Venues.js
+++ src/components/pages/Venues.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+const VENUES_URL = 'https://api.noroff.dev/api/v1/holidaze/venues';
 
 function Venues() {
+  const [venues, setVenues] = useState([]);
+
+  // The useEffect will run once when the component first mounts
+  useEffect(() => {
+    async function fetchVenues() {
+      // Function that gets our posts from the API
+      const response = await fetch(VENUES_URL);
+      const json = await response.json();
+
+      // PI data we received, setting our venues state
+      setVenues(json);
+    }
+    fetchVenues();
+  }
+  , []);
+
   return (
     <div>
-      <h1>Venues</h1>
-      <p>List of Venues</p>
+      {venues.map((venue) => (
+        <div key={venue.id}>
+          <h2>{venue.name}</h2>
+          <img src={venue.media[0]} alt={venue.name} />
+          <p>{venue.description}</p>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
 export default Venues;
