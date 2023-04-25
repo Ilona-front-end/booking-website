8e82c73f578119e40dd6a9bba445acc6e13d698f
Ilona R <ilona.raugalaite@yahoo.co.uk>
2023-04-25 21:09:42 +0200

    HOT-27 profile page - avatar update
    1. fetch user profile
    2.update user avatar if user wants

--- src/components/pages/Profile.js
+++ src/components/pages/Profile.js
@@ -6,6 +6,8 @@ function Profile() {
   const userName = localStorage.getItem('user');
 
   const [userProfile, setProfile] = useState(null);
+  const [newAvatarUrl, setNewAvatarUrl] = useState('');
+  const [showOptionToChangeAvatar, setShowOptionToChangeAvatar] = useState(false);  // showOptionToChangeAvatar has 2 options: weather update btn visible; weather input and save btn visible
 
   useEffect(() => {
     async function getUserProfile() {
@@ -51,7 +53,40 @@ function Profile() {
     getUserProfile();
   }, [userName, token]);
 
-  // const { name, email, venueManager, avatar } = userProfile;
+  // destructuring causing errors after reload; const { name, email, venueManager, avatar } = userProfile;
+
+  // show input field and save btn after clicking update btn
+  const handleAvatarUpdateBtn = () => {
+    setShowOptionToChangeAvatar(true);
+  };
+
+  // cancel btn to stop updating avatar
+  const handleAvatarCancelBtn = () => {
+    setShowOptionToChangeAvatar(false);
+    setNewAvatarUrl('');
+  };
+
+  // user put new url and clicks on button save avatar, now sending new avatar to API
+  const handleAvatarSubmit = async () => {
+    try {
+      const response = await fetch(PROFILE_BASE_URL + userName + '/media', {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({ 
+          avatar: newAvatarUrl 
+        }),
+      });
+      const json = await response.json();
+      console.log('line 79', json)
+      setProfile(json);
+      setShowOptionToChangeAvatar(false);
+    } catch {
+      console.log('error');
+    }
+  };
 
   return (
     <>
@@ -66,6 +101,17 @@ function Profile() {
           </div>
           <div className="mt-5 mb-8 border-t border-gray-200">
             <dl className="divide-y divide-gray-200">
+              <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5">
+                <dt className="text-sm font-medium text-gray-500">
+                  <img src={userProfile?.avatar} alt={userProfile?.name} className="h-24 w-24 rounded-full"/>
+                </dt>
+                <dd className="mt-1 flex text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                  <span className="flex-grow"></span>
+                  <span className="ml-4 flex-shrink-0">
+                    <span className="rounded-md bg-white font-medium text-red-700"></span>
+                  </span>
+                </dd>
+              </div>
               <div className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5">
                 <dt className="text-sm font-medium text-gray-500">User name</dt>
                 <dd className="mt-1 flex text-sm text-gray-900 sm:col-span-2 sm:mt-0">
@@ -98,15 +144,67 @@ function Profile() {
                   Profile picture
                 </dt>
                 <dd className="mt-1 flex text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-                  <div className="flex-grow"><img src={userProfile?.avatar} alt={userProfile?.name} /></div>
-                  <span className="ml-4 flex-shrink-0">
+                  <span className="flex-grow"></span>
+                  {showOptionToChangeAvatar ? (
+                    <>
+                    {/* step 2: show input field and save button */}
+                      <span className="flex-grow">
+                        <label htmlFor="avatarInput">
+                          <input
+                            type="text"
+                            name="avatarInput"
+                            className="block w-full rounded-md bg-slate-200 py-2 px-2 border-gray-600 shadow-sm focus:border-indigo-800 focus:ring-indigo-700 sm:text-sm"
+                            value={newAvatarUrl}
+                            onChange={(e) => setNewAvatarUrl(e.target.value)}
+                          />
+                        </label>
+                      </span>
+                      <button 
+                        type="button" 
+                        className="rounded-md bg-white ml-3 py-2 px-2 font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2" 
+                        onClick={handleAvatarSubmit}
+                      >
+                        Save
+                      </button>
+                      {/* cancel button */}
+                      <button 
+                        type="button" 
+                        className="rounded-md ml-3 py-2 px-2 bg-white font-medium text-gray-500 hover:text-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2" 
+                        onClick={handleAvatarCancelBtn}
+                      >
+                        Cancel
+                      </button>
+                      {/* <span className="flex-grow">
+                        <label htmlFor="avatarInput">
+                          <input
+                            type="text"
+                            name="avatarInput"
+                            className="block w-full rounded-md bg-slate-200 py-2 px-2 border-gray-600 shadow-sm focus:border-indigo-800 focus:ring-indigo-700 sm:text-sm"
+                            value={newAvatarUrl}
+                            onChange={(e) => setNewAvatarUrl(e.target.value)}
+                          />
+                        </label>
+                      </span> */}
+                    </>
+                  ) : (
+                    // step 1 - show button update
+                    <button
+                      type="button"
+                      className="rounded-md bg-white font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                      onClick={handleAvatarUpdateBtn}
+                    >
+                      Update
+                    </button>
+                  )
+                  }
+                  {/* <span className="ml-4 flex-shrink-0">
                     <button
                       type="button"
                       className="rounded-md bg-white font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
                       Update
                     </button>
-                  </span>
-                  <div className="hidden flex-grow">
+                  </span> */}
+                  {/* <div className="hidden flex-grow">
                     <label htmlFor="avatar1">
                       <input
                         type="text"
@@ -115,8 +213,8 @@ function Profile() {
                         value=""
                       />
                     </label>
-                  </div>
-                  <span
+                  </div> */}
+                  {/* <span
                     className="hidden ml-4 flex-shrink-0"
                   >
                     <button
@@ -125,7 +223,7 @@ function Profile() {
                     >
                       Change
                     </button>
-                  </span>
+                  </span> */}
                 </dd>
               </div>
             </dl>
