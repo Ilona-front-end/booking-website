579172c7860829b938ac724424d4ca63a01b0fa9
Ilona R <ilona.raugalaite@yahoo.co.uk>
2023-04-27 19:31:14 +0200

    header burger menu add

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,12 @@ module.exports = {
       sans: ['Montserrat', 'sans-serif'],
       serif: ['Roboto Condensed', 'serif'],
     },
+    screens: {
+      xs: { min: '0px', max: '370px' },
+      sm: { min: '371px', max: '767px' },
+      md: { min: '768px', max: '1023px' },
+      lg: { min: '1024px' },
+    },
   },
   plugins: [],
-}
+};
