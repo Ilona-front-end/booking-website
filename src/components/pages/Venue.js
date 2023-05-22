dcb2f57063e47631b034526a7902fbcd9b5d8e41
Ilona R <ilona.raugalaite@yahoo.co.uk>
2023-05-22 13:45:36 +0200

    HOT-43 venue add description, details, owner, booking history
    missing posibility to book venue
    
    need to change loader, experiencing problems to store data because component is rendered before the fetch operation is completed

--- src/components/pages/Venue.js
+++ src/components/pages/Venue.js
@@ -5,12 +5,16 @@ import ErrorMessage from '../shared/ErrorMessage';
 import RenderStars from '../../utils/renderStars';
 import { FiCheckSquare } from 'react-icons/fi';
 import { TbSquare } from 'react-icons/tb';
+import { FaUserCircle } from 'react-icons/fa';
+import { mapTime } from '../../utils/mapTime';
+import { formateDate } from '../../utils/dateFormatting';
 
 export default function Venue() {
   // useParams() hook from React Router library allows to get the id from the URL, route is configured as '/venues/:id' that allows to retrieve the id from the URL
   const { id } = useParams();
 
   const [venue, setVenue] = useState({});
+  const [isLoading, setIsLoading] = useState(true); // We will use this state to show a loading indicator
   const [error, setError] = useState(null);
 
   async function fetchVenue() {
@@ -25,12 +29,14 @@ export default function Venue() {
       }
       const venueResponse = await response.json();
       setVenue(venueResponse);
-      console.log('Venue information is fetched', venueResponse);
+      console.log('Venue information is fetched venueResponse', venueResponse);
       setError(null);
     } catch (error) {
       setError(error.message);
       console.error('Error message here: ', error);
       console.error('Error here: ', error.message); // "error.message":"Invalid uuid" not helpful for user
+    } finally {
+      setIsLoading(false); // We are done with the request, no matter if we got an error or not
     }
   }
 
@@ -39,22 +45,36 @@ export default function Venue() {
     fetchVenue();
   }, []);
 
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
   // Error message
   // if (error) {
   //   return <ErrorMessage errorText={error} />;
   // }
 
-  const { name, price, rating, media, description, meta } = venue;
+  console.log('Venue information is fetched venue', venue);
+  const {
+    name,
+    price,
+    rating,
+    media,
+    location,
+    description,
+    meta,
+    owner,
+    bookings,
+  } = venue;
 
   return (
     <div className="wrapper-max-width wrapper-padding-x">
-      {/* <div class="pb-16 pt-6 sm:pb-24">
-          <div class="mx-auto mt-8 max-w-2xl px-4 sm:px-6 lg:max-w-7xl lg:px-8"> */}
       <div className="pb-16 pt-6 sm:pb-24">
         <div>
-          {/* <!-- Error message --> */}
+          {/*  Error message */}
           {error && <ErrorMessage errorText={error} />}
-          {/* <!-- Venue --> */}
+
+          {/*  Venue */}
           <div className="lg:grid lg:auto-rows-min lg:grid-cols-12 lg:gap-x-8">
             <div className="lg:col-span-5 lg:col-start-8">
               {/* <!-- Name and Price --> */}
@@ -62,25 +82,25 @@ export default function Venue() {
                 <h1 className="text-xl font-medium text-gray-900">{name}</h1>
                 <p className="text-xl font-medium text-gray-900">{price} NOK</p>
               </div>
-              {/* <!-- Ratings --> */}
+              {/*  Ratings */}
               <div className="mt-4">
-                <h2 className="sr-only">Ratings</h2>
+                <p className="sr-only">Ratings</p>
                 <div className="flex items-center">
                   <p className="text-sm text-gray-700">
                     {rating}
                     <span className="sr-only"> out of 5 stars</span>
                   </p>
                   <div className="ml-1 flex items-center">
-                    {/* <!-- Active: "text-yellow-400", Inactive: "text-gray-200" --> */}
+                    {/* Active: "text-yellow-400", Inactive: "text-gray-200" */}
                     <RenderStars rating={rating} />
                   </div>
                 </div>
               </div>
             </div>
 
-            {/* <!-- Image gallery --> */}
+            {/*  Image gallery */}
             <div className="mt-8 lg:col-span-7 lg:col-start-1 lg:row-span-3 lg:row-start-1 lg:mt-0">
-              <h2 className="sr-only">Images</h2>
+              <p className="sr-only">Images</p>
 
               <div className="grid grid-cols-1 lg:grid-cols-2 lg:grid-rows-2 lg:gap-8">
                 {media && media.length > 0 && (
@@ -109,70 +129,52 @@ export default function Venue() {
               </div>
             </div>
 
-            <div class="mt-8 lg:col-span-5">
-              {/* <!-- Venue details --> */}
-              <div class="mt-10">
-                <h2 class="text-sm font-medium text-gray-900">Description</h2>
-
-                <div class="text-sm mt-4 text-gray-500">
-                  <p>{description}</p>
+            <div className="mt-8 lg:col-span-5">
+              {/*  Venue details */}
+              <div className="mt-10">
+                {/*  Location */}
+                <div className="mb-2">
+                  {location &&
+                    location.city &&
+                    location.country &&
+                    location.city !== 'Unknown' &&
+                    location.city !== 'unknown' &&
+                    location.city !== 'string' &&
+                    location.city !== '' &&
+                    location.city !== 'test' && (
+                      <span className="text-sm text-gray-700">
+                        {location.city}
+                      </span>
+                    )}
+                  {location &&
+                    location.city &&
+                    location.country &&
+                    location.city !== 'Unknown' &&
+                    location.city !== 'unknown' &&
+                    location.city !== 'string' &&
+                    location.city !== '' &&
+                    location.city !== 'test' &&
+                    location.country !== 'Unknown' &&
+                    location.country !== 'unknown' &&
+                    location.country !== 'string' &&
+                    location.country !== '' &&
+                    location.country !== 'test' && <span>, </span>}
+                  {location &&
+                    location.country &&
+                    location.country !== 'Unknown' &&
+                    location.country !== 'unknown' &&
+                    location.country !== 'string' &&
+                    location.country !== '' &&
+                    location.country !== 'test' && (
+                      <span className="text-sm text-gray-700">
+                        {location.country}
+                      </span>
+                    )}
                 </div>
-              </div>
 
-              <div class="mt-8 border-t border-gray-200 pt-8">
-                <h2 class="text-sm font-medium text-gray-900">Details</h2>
-
-                <div class="text-sm mt-4 text-gray-500">
-                  <div>
-                    <p className="mt-1 flex items-center text-sm leading-6 text-gray-900">
-                      Wifi:
-                      {meta?.wifi ? (
-                        <span className="pl-2">
-                          <FiCheckSquare />
-                        </span>
-                      ) : (
-                        <span className="pl-2">
-                          <TbSquare />
-                        </span>
-                      )}
-                    </p>
-                    <p className="mt-1 flex items-center text-sm leading-6 text-gray-900">
-                      Parking:
-                      {meta?.parking ? (
-                        <span className="pl-2">
-                          <FiCheckSquare />
-                        </span>
-                      ) : (
-                        <span className="pl-2">
-                          <TbSquare />
-                        </span>
-                      )}
-                    </p>
-                    <p className="mt-1 flex items-center text-sm leading-6 text-gray-900">
-                      Breakfast:
-                      {meta?.breakfast ? (
-                        <span className="pl-2">
-                          <FiCheckSquare />
-                        </span>
-                      ) : (
-                        <span className="pl-2">
-                          <TbSquare />
-                        </span>
-                      )}
-                    </p>
-                    <p className="mt-1 flex items-center text-sm leading-6 text-gray-900">
-                      Pets:
-                      {meta?.pets ? (
-                        <span className="pl-2">
-                          <FiCheckSquare />
-                        </span>
-                      ) : (
-                        <span className="pl-2">
-                          <TbSquare />
-                        </span>
-                      )}
-                    </p>
-                  </div>
+                <p className="text-sm font-medium text-gray-900">Description</p>
+                <div className="text-sm mt-4 text-gray-500">
+                  <p>{description}</p>
                 </div>
               </div>
 
@@ -181,9 +183,118 @@ export default function Venue() {
                   type="submit"
                   class="mt-8 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                 >
-                  Add to cart
+                  Book venue
                 </button>
               </form>
+
+              <div className="mt-8 border-t border-gray-200 pt-8">
+                <p className="text-sm font-medium text-gray-900">Details</p>
+                {/* meta: wifi, parking, breakfast, pets */}
+                <div className="mt-4">
+                  <p className="mt-1 flex items-center text-sm leading-6 text-gray-700">
+                    Wifi:
+                    {meta.wifi ? (
+                      <span className="pl-2">
+                        <FiCheckSquare />
+                      </span>
+                    ) : (
+                      <span className="pl-2">
+                        <TbSquare />
+                      </span>
+                    )}
+                  </p>
+                  <p className="mt-1 flex items-center text-sm leading-6 text-gray-700">
+                    Parking:
+                    {meta.parking ? (
+                      <span className="pl-2">
+                        <FiCheckSquare />
+                      </span>
+                    ) : (
+                      <span className="pl-2">
+                        <TbSquare />
+                      </span>
+                    )}
+                  </p>
+                  <p className="mt-1 flex items-center text-sm leading-6 text-gray-700">
+                    Breakfast:
+                    {meta.breakfast ? (
+                      <span className="pl-2">
+                        <FiCheckSquare />
+                      </span>
+                    ) : (
+                      <span className="pl-2">
+                        <TbSquare />
+                      </span>
+                    )}
+                  </p>
+                  <p className="mt-1 flex items-center text-sm leading-6 text-gray-700">
+                    Pets:
+                    {meta.pets ? (
+                      <span className="pl-2">
+                        <FiCheckSquare />
+                      </span>
+                    ) : (
+                      <span className="pl-2">
+                        <TbSquare />
+                      </span>
+                    )}
+                  </p>
+                </div>
+              </div>
+              {/* Owner */}
+              <div className="mt-8 border-t border-gray-200 pt-8">
+                <p className="text-sm font-medium text-gray-900">Owner</p>
+                <div className="text-sm text-gray-700 mt-4 flex items-center">
+                  {owner.name}
+                  <span>
+                    {owner.avatar ? (
+                      <img
+                        className="rounded-full w-6 h-6 ml-3"
+                        src={owner.avatar}
+                        alt={owner.name}
+                      />
+                    ) : (
+                      <FaUserCircle className="w-6 h-6 ml-3" />
+                    )}
+                  </span>
+                </div>
+                <div className="text-sm text-gray-700 mt-1">{owner.email}</div>
+              </div>
+
+              {/* Booking history */}
+              <div className="mt-8 border-t border-gray-200 pt-8">
+                <p className="text-sm font-medium text-gray-900">
+                  Booking history
+                </p>
+
+                <div className="text-sm text-gray-700 mt-4">
+                  Total number: {bookings.length}
+                </div>
+                <div className="text-sm text-gray-700 mt-2">
+                  <div className="space-y-6">
+                    {bookings.map((booking) => (
+                      <div className="relative flex gap-x-4">
+                        <div className="absolute left-0 top-0 flex w-6 justify-center -bottom-6">
+                          <div className="w-px bg-gray-200"></div>
+                        </div>
+                        <div className="relative flex h-6 w-6 flex-none items-center justify-center bg-white">
+                          <div className="h-1.5 w-1.5 rounded-full bg-gray-100 ring-1 ring-gray-300"></div>
+                        </div>
+                        <p className="flex-auto py-0.5 text-xs leading-5 text-gray-500">
+                          <span className="font-medium text-gray-900">
+                            User:{' '}
+                          </span>
+                          {formateDate(booking.dateFrom)} {'-'}{' '}
+                          {formateDate(booking.dateTo)}
+                        </p>
+                        <time className="flex-none py-0.5 text-xs leading-5 text-gray-500">
+                          {mapTime(booking.created)} ago
+                        </time>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
